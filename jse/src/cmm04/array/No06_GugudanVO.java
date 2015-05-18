ecb0318809c5fdc92e8b90e0e3ba72f63320ccb9
Administrator <Administrator@MSDN-SPECIAL>
2015-05-18 09:47:34 +0900

    5.18

--- jse/src/cmm04/array/No06_GugudanVO.java
+++ jse/src/cmm04/array/No06_GugudanVO.java
@@ -4,16 +4,16 @@ public class No06_GugudanVO {
 	public void calc(){
 		for (int i = 1; i < 10; i++) {
 			for (int j = 2; j < 6; j++) {
-				System.out.print(j + " * " + i + " = " + (i * j));
+				System.out.print(j + " * " + i + " = " + j*i);
 				System.out.print("\t");
 			}
 			System.out.println();
 
 		}
-		System.out.println();
+		System.out.println("");
 		for (int i = 1; i < 10; i++) {
 			for (int j = 6; j < 10; j++) {
-				System.out.print(j + " * " + i + " = " + (i * j));
+				System.out.print(j + " * " + i + " = " + i * j);
 				System.out.print("\t");
 			}
 			System.out.println();	
