4bfd28c59ae8a6d9caf96c984a923286dfb5f57e
Administrator <Administrator@MSDN-SPECIAL>
2015-05-21 11:55:02 +0900

    5.21

--- jse/src/oop01_syntax/CardVO.java
+++ jse/src/oop01_syntax/CardVO.java
@@ -1,8 +1,8 @@
 package oop01_syntax;
 
 class CardVO {
-	String kind;  // 카드의 무늬
-	int number;   // 카드의 숫자
+	private String kind;  // 카드의 무늬
+	private int number;   // 카드의 숫자
 	static int width = 150;  // 카드의 너비
 	static int height = 300; // 카드의 높이
 
---
+++ jse/src/oop01_syntax/KaupMain.java
@@ -0,0 +1,27 @@
+package oop01_syntax;
+
+import java.util.Scanner;
+
+public class KaupMain {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("키를 입력해 주세요.");
+		double height = scanner.nextDouble();
+		System.out.println("몸무게를 입력해 주세요.");
+		double weight = scanner.nextDouble();
+		KaupVO vo = new KaupVO(height, weight);
+		vo.setHeight(height);
+		vo.setWeight(weight);
+		//vo.setIdx(height,weight);
+		
+		
+		/*int idx=0;
+		vo.setIdx(idx);
+		vo.getMsg();*/
+		
+		
+		System.out.println(vo.toString());
+		System.out.println("완료");
+		
+	}
+}
