2ef2afb5823eb2a92395db592932880fe18d024e
dkjeong0815 <dkjeong0815@gmail.com>
2015-05-26 11:54:14 +0900

    jse 2016..

---
+++ jse/src/oop07/nestedClass/InnerClassAnonyServiceImpl.java
@@ -0,0 +1,16 @@
+package oop07.nestedClass;
+
+public class InnerClassAnonyServiceImpl {
+	String data = "Hello Anonymous World !!";
+	public void print(){
+		new InnerClassAnonyService(){
+
+			@Override
+			public void printData() {
+				System.out.println("익명 내부 클래스 출력 :" + data);
+				
+			}
+			
+		}.printData();
+	}
+}
